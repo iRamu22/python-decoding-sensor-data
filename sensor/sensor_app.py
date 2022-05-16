8494772be5e7c166e4fe2dec375284cc821cbc15
iRamu22 <GlobalLogic@987>
2022-05-16 17:47:41 +0530

    Completed module - immersion program

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -64,6 +64,8 @@ print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 # Module 6 code here:
 recs = particle_data.get_data_by_date(rec_date=test_date)
 print("\nHouse Particle sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
+
+concentrations = particle_data.get_data_concentrations(data=recs)
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
